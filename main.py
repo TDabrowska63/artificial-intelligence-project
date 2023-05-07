6256ebf9f03f59e593330eb25b221f2b87f57972
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-07 16:42:53 +0200

    nowa zmiana

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def test:
+def test():
+    print("miki jest serio dziki")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 print("hello miki")
-
+test()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
