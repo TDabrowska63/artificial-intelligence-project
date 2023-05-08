56552a3d099869b74ff48226c2dfab1684f62d86
Mikolaj Bisewski <mikolajbisewski@wp.pl>
2023-05-08 13:35:29 +0200

    Added randomization of adjacency matrix, removed unnecesary code

--- CustomGraph.py
+++ CustomGraph.py
@@ -1,15 +1,20 @@
 import numpy as np
+import random
 
 
 class CustomGraph:
-    def __int__(self, type="adjmatrix", numberOfNodes=10):
+    def __init__(self, numberOfNodes=10):
         self.numberOfNodes = numberOfNodes
-        if type == "adjmatrix":
-            self.struct = np.zeros(numberOfNodes);
-        elif type == "adjlist":
-            pass
-            # TODO Implement adjacency list construction
-            # I still dont know if this is necessary
+        self.matrix = np.zeros((numberOfNodes, numberOfNodes), dtype=int)
 
+    # Method just for debugging, ignore
     def printMe(self):
-        print(self.struct)
+        print(self.matrix)
+
+    def randomize(self):
+        for i in range(self.numberOfNodes):
+            for j in range(i + 1, self.numberOfNodes):
+                tmp = random.randint(0, 1)
+                self.matrix[i, j] = tmp
+                self.matrix[j, i] = tmp
+
