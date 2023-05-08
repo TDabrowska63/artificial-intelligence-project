2f49c87c08c4de6d37a941135439e1792837dabb
Mikolaj Bisewski <mikolajbisewski@wp.pl>
2023-05-08 12:01:22 +0200

    Added new class "Graph"

---
+++ .idea/Graph.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class graph:
+    def __int__(self, type = "adjmatrix", numberOfNodes = 10):
+        self.numberOfNodes = numberOfNodes
+        if type == "adjmatrix":
+            pass
+            #TODO
+            # Implement adjacency matrix construction
+        elif type == "adjlist":
+            pass
+            #TODO
+            # Implement adjacency list construction
+            # I still dont know if this is necessary
+
