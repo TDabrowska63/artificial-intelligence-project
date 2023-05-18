81003e6c18097a484b4e020746c987075212b98a
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-18 13:55:15 +0200

    i have no idea

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from CustomGraph import *
 from GUI.MainWindow import MainWindow
 from GUI.Gui import Gui
 import customtkinter as ctk
+from Astar import *
 
 g = CustomGraph(10, 30)
 g.randomize()
-gui = Gui(g)
 g.printMe()
-
+a = Astar(g)
+a.printMe()
+path = a.aStarAlgorithm(0, 9)
+gui = Gui(g)
