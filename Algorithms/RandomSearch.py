24b9c675cbca250c67fff2c7da260492990cf01a
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-27 19:47:42 +0200

    implemented another version of random search, not yet tested.

--- Algorithms/RandomSearch.py
+++ Algorithms/RandomSearch.py
@@ -1,32 +1,33 @@
 import sys
 import random
+from Misc.CustomGraph import CustomGraph
 import numpy as np
+from Misc import *
 
 class RandomSearch:
-    def __init__(self, graph):
+    def init(self, graph):
         self.graph = graph
+        self.adjList = graph.transform(graph)
 
-    def random_search(self,start_node, end_node, max_iterations):
+    def randomSearch(self, start_node, end_node, iterations):
         current_node = start_node
-        path = [current_node]
-        total_weight = 0
 
-        for i in range(max_iterations):
-            if current_node == end_node:
-                return path, total_weight
-
-            neighbors = self.graph[current_node]
-            neighbor_nodes = [i for i in range(len(neighbors)) if neighbors[i] != 0]
-            neighbor_weights = [neighbors[i] for i in neighbor_nodes]
-
-            random_index = random.randint(0, len(neighbor_nodes) - 1)
-            next_node = neighbor_nodes[random_index]
-            next_weight = neighbor_weights[random_index]
+        bestDistance = float('inf')
+        bestIteration = 1
+        path = [start_node]
+        for i in range(iterations):
+            next_node = random.choice(self.adjList[current_node])
             path.append(next_node)
-            total_weight += next_weight
 
             current_node = next_node
+            if current_node == end_node:
+                distance = 0
+                for j in range(len(path) - 1):
+                    distance = distance + self.graph.weighmatrix[path[i], path[i + 1]]
+                if distance < bestDistance:
+                    bestDistance = distance
+                    bestPath = path[:]
+                    bestIteration = i
+                path = [start_node]
 
-        return None
-
-
+        return bestDistance, bestPath, bestIteration
