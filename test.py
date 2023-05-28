44e257bb149e36d715777ac6bed28f50c83769b5
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-28 17:45:24 +0200

    implemented clicking on the map

--- test.py
+++ test.py
@@ -4,21 +4,46 @@ from Algorithms.Astar import Astar
 from Misc.CustomGraph import *
 
 import osmnx as ox
-import folium
-import networkx as nx
-from math import sin, cos, sqrt, atan2, radians
+
+
+class Test:
+    def __init__(self, g):
+        self.node1 = None
+        self.node2 = None
+        self.g = g
+        self.shortestRoute = []
+
+    def mouse_event(self, event):
+        nid = ox.nearest_nodes(self.g.mapReference, event.xdata, event.ydata)
+        node_id = list(self.g.mapReference.nodes)
+        print('x: {} and y: {}'.format(event.xdata, event.ydata))
+        for i in range(len(node_id)):
+            if node_id[i] == nid:
+                if self.node1 is None:
+                    self.node1 = i
+                elif self.node2 is None:
+                    self.node2 = i
+                break
+
+        if self.node1 is not None and self.node2 is not None:
+            a = Astar(self.g)
+            distance, path, states_matrix = a.aStarAlgorithm(self.node1, self.node2)
+            node_id = list(self.g.mapReference.nodes)
+            for p in path:
+                self.shortestRoute.append(node_id[p])
+
+    def getShortestRoute(self):
+        return self.shortestRoute
 
 
 
-def mouse_event(event):
-    print('x: {} and y: {}'.format(event.xdata, event.ydata))
 
-
-g = CustomGraph(place="Poland, Rumia")
+g = CustomGraph(place="Santa Monica, California")
+t = Test(g)
 fig, ax = ox.plot_graph(g.mapReference, close=False, show=False)
-cid = fig.canvas.mpl_connect('button_press_event', mouse_event)
+cid = fig.canvas.mpl_connect('button_press_event', t.mouse_event)
 plt.show()
-
+ox.plot_graph_route(g.mapReference, t.shortestRoute)
 
 
 # a = Astar(g)
